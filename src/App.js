834cef459f58b5d1527ee890a48d3531d913cade
unknown <tarekyosry8@gmail.com>
2023-06-19 13:34:01 +0300

    app styling

--- src/App.js
+++ src/App.js
@@ -3,17 +3,17 @@ import 'office-ui-fabric-react/dist/css/fabric.css';
 import './App.css';
 import Navigation from './Navigation';
 import CardSection from './CardsSection';
-import { Card } from '@uifabric/react-cards';
+// import { Card } from '@uifabric/react-cards';
 
 function App() {
   return (
 
       <div className='ms-Grid' dir='ltr'>
         <div className='ms-Grid-row'>
-          <div className='ms-Grid-col ms-sm-1 ms-xl1'>
+          <div className='ms-Grid-col ms-sm1 ms-xl1'>
             <Navigation />
           </div>
-          <div className='ms-Grid-col ms-sm1 ms-xl1 main-element'>
+          <div className='ms-Grid-col ms-sm11 ms-xl11 main-element'>
             <div className='ms-Grid-row'>
               <CardSection />
             </div>
