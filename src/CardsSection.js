dd11f1ea8793a55f9b54d52b68becbc6687942e9
unknown <tarekyosry8@gmail.com>
2023-06-19 13:34:29 +0300

    finish CardSection

--- src/CardsSection.js
+++ src/CardsSection.js
@@ -1,7 +1,54 @@
 import React from "react";
-import {Card} from "@uifabric/react-cards";
+import { Card } from "@uifabric/react-cards";
 import { Text, initializeIcons } from "@fluentui/react";
 import 'office-ui-fabric-react/dist/css/fabric.css';
+
+const container = {
+    display: 'flex',
+    justifyContent: 'center',
+    margin: '10vh 0',
+};
+
+const styles = {
+    cardStyles: {
+        root: {
+            backgroundColor: 'white',
+            padding: 20,
+            borderTop: '5px solid #0078d4',
+            width: '90%',
+            maxWidsh: '90%',
+            margin: 'auto'
+        }
+    },
+    header: {
+        root: {
+            fontSize: 20,
+            fontWeight: 'bold',
+        }
+    },
+    amount: {
+        root: {
+            fontSize: 26,
+            paddingBottom: 20,
+            paddingTop: 30,
+        }
+    },
+    percentage: {
+        root: {
+            fontSize: 16,
+            fontWeight: 'bold',
+            color: '#0078d4',
+        }
+    }
+}
+
+const icon = {
+    fontSize: 24,
+    padding: 15,
+    verticalAlign: 'middle',
+    paddingLeft: 0,
+    color: '#0078d4'
+}
 
 const cards = [
     {
@@ -27,19 +74,25 @@ const cards = [
 const CardSection = () => {
     initializeIcons();
     return (
-        <div>
-            {cards.map((card) => {
-                <div className="s-Grid-col ms-sm-3 ms-xl-3">
-                    <Card>
+        <div style={container}>
+            {cards.map((card) => (
+                <div className="s-Grid-col ms-sm3 ms-xl3">
+                    <Card styles={styles.cardStyles}>
                         <Card.Section>
                             <Card.Item>
-                                <i className={`ms-Icon ms-Icon--${card.icon}`}aria-hidden="true"></i>
-                                <Text>{card.title}</Text>
+                                <i style={icon} className={`ms-Icon ms-Icon--${card.icon}`}aria-hidden="true"></i>
+                                <Text styles={styles.header}>{card.title}</Text>
+                            </Card.Item>
+                            <Card.Item>
+                                <Text styles={styles.amount}>{card.amount}</Text>
+                            </Card.Item>
+                            <Card.Item>
+                                <Text styles={styles.percentage}>{card.percentage}%</Text>
                             </Card.Item>
                         </Card.Section>
                     </Card>
                 </div>
-            })}
+            ))}
         </div>
     )
 }
